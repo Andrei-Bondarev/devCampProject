f95b1613ab9316fc1bbf008703431fad5e70229e
Andrei Bondarev <Andrei14bondarev@gmail.com>
2021-12-09 17:25:30 +0200

    the api project was configured

---
+++ api/src/app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+var express = require("express");
+var app = express();
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+app.listen(process.env.DB_PORT, function () {
+  console.log("Example app listening on port 3000!");
+});
