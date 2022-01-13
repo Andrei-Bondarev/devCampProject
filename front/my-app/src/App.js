8f9d0e625e2b91f1b84433b3d434215262ba75d8
Andrei Bondarev <Andrei14bondarev@gmail.com>
2022-01-13 14:40:42 +0200

    added routing for Posts,AddPost,Profile

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import HeaderContainer from "./containers/header/HeaderContainer";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <HeaderContainer/>
-        </div>
-    );
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HeaderContainer startpage='Profile'/>}/>
+                <Route path="/Profile" element={<HeaderContainer startpage='Profile'/>}/>
+                <Route path="/Posts" element={<HeaderContainer startpage='Posts'/>}/>
+                <Route path="/Posts/add" element={<HeaderContainer startpage='AddPost'/>}/>
+                <Route path="/Posts/:id" element={<HeaderContainer startpage='Post'/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+        ;
 }
 
 export default App;
